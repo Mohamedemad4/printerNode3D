1dbf2e547406d60affdc4f26110c72619a597f7a
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2018-10-02 11:28:01 +0200

    non Working Websockets

--- client/main.go
+++ client/main.go
@@ -1,3 +1,4 @@
+//See how to make websocket conversion
 package main
 
 import (
@@ -6,18 +7,21 @@ import (
 "bytes"
 "io"
 "strings"
+"net/http"
 "io/ioutil"
+"strconv"
 "log"
 "net"
 "github.com/gorilla/mux"
-"net/http")
+"github.com/gorilla/websocket")
 
 var nodeServer="localhost:8080";
+var upgrader = websocket.Upgrader{}
 
 func main(){
     router := mux.NewRouter()
     router.HandleFunc("/",RootPage).Methods("GET")
-    router.HandleFunc("/s",FileUpload).Methods("POST")
+    router.HandleFunc("/",FileUpload).Methods("POST")
   
     log.Println("Serving on :5000")
     log.Fatal(http.ListenAndServe(":5000",router))
@@ -50,15 +54,12 @@ func FileUpload(w http.ResponseWriter, r *http.Request){
     io.Copy(&Buf, file)
 
     contents := Buf.String()
-    Status:=uploadCode(contents,"M23","ss.gco")
-
-    fmt.Fprintf(w,Status)
+    uploadCode(contents,"M23","ss.gco",w,r)
 }
 
-func uploadCode(file string,FCode string,FileName string) string{
+func uploadCode(file string,FCode string,FileName string,w http.ResponseWriter,r *http.Request){
 	var commands int
 	var speedmode bool
-
 
 	GcodeSplit:=strings.Split(file, "\n")
 	GcodeSplitLen:=len(strings.Split(file, "\n"))
@@ -67,20 +68,29 @@ func uploadCode(file string,FCode string,FileName string) string{
 
 	if err!=nil{
 		log.Println(err)
-		return "Could not connect to the Node Please check your connection"
+		fmt.Fprintf(w,"Could not connect to the Node Please check your connection")
 	}
 	if (FCode=="M928"){
 		fmt.Fprintf(conn,"M928 %s \n",FileName) // todo: unsure about this test it please
 	}else{
 		fmt.Fprintf(conn,"M28 %s \n",FileName)
 	}
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		fmt.Fprintf(w,"Error Please Check the Logs")
+	}
+
+	defer c.Close()
     for index, element := range GcodeSplit{
     	fmt.Fprintf(conn,element+"\n") 
     	commands+=1
 
         if (commands==500){ //100,80
             commands=0
-            log.Println(float32(index)/float32(GcodeSplitLen))
+            perc:=[]byte(strconv.FormatFloat(float64(index)/float64(GcodeSplitLen)*10, 'f', 6, 64))
+            c.WriteMessage(10,perc)
             speedmode=false
         }else{
             speedmode=true
@@ -96,5 +106,5 @@ func uploadCode(file string,FCode string,FileName string) string{
    		fmt.Fprintf(conn,"M23 %s \n",FileName)
    }
    conn.Close()
-   return "200ok"
+   fmt.Fprintf(w,"200ok")
 }
